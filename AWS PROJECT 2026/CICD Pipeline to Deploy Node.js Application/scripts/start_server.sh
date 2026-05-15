85af58b57e57a9700fb0cd15205f4fe12a26afeb
sushil yeole <sushyeole04@gmail.com>
2026-05-15 09:17:37 +0530

    Initial commit

--- AWS PROJECT 2026/CICD Pipeline to Deploy Node.js Application/scripts/start_server.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-
-cd /home/ec2-user/app
-# Start the server (assuming server.js is the entry point, update if different)
-pm2 start server.js --name "node-app"
-pm2 save
-pm2 startup | grep "sudo env" | bash || true
